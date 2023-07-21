629baa1f44f3f43bbcb0e9e80dc6030669d9415f
AtlasOfArthur <arthur.harjama@gmail.com>
2023-07-21 19:31:11 +0300

    JS ei toimi vaikka oikein tehty. en tiedä miksi

---
+++ Graafinen käyttöliittymä projekti/GKL-labelJs.js
@@ -0,0 +1,10 @@
+const labels = document.querySelectorAll('.input-box label');
+
+labels.forEach(label => {/* Get text, split to empty('')array, map letters by index */
+    label.innerHTML = label.innerText.split('').map((letter, index) => {
+        return `<span>${letter}</span>` /* Palauttaa jokaisen kirjaimen erikseen 'jaksoina'(span) */
+    }).join('') /* Palauttaa kirjain jakson takaisin taulukoksi(array) */
+})
+
+/* Selaimessa: Hiiren oikea, inspect, (Pick element) label >> span */
+/* CSS animaatio >> .input-box input:focus + label span, .input-box input:valid + label span */
