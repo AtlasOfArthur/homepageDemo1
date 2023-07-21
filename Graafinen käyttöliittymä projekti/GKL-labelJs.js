06c0787cb41ed8741d674258fd4e3689b6ba4c45
AtlasOfArthur <arthur.harjama@gmail.com>
2023-07-21 19:59:45 +0300

    .

--- Graafinen käyttöliittymä projekti/GKL-labelJs.js
+++ Graafinen käyttöliittymä projekti/GKL-labelJs.js
@@ -1,9 +1,10 @@
 const labels = document.querySelectorAll('.input-box label');
 
-labels.forEach(label => {/* Get text, split to empty('')array, map letters by index */
-    label.innerHTML = label.innerText.split('').map((letter, index) => {
-        return `<span>${letter}</span>` /* Palauttaa jokaisen kirjaimen erikseen 'jaksoina'(span) */
-    }).join('') /* Palauttaa kirjain jakson takaisin taulukoksi(array) */
+labels.forEach(label => {
+    console.log(label)/*v Get text, split to empty('')array, map letters by index */
+    label.innerHTML = label.innerText.split('').map((letter, index) => 
+        `<span style="transition-delay:${index*50}ms">${letter}</span>`) /* Palauttaa jokaisen kirjaimen erikseen 'jaksoina'(span) */
+        .join('') /* Palauttaa kirjain jakson takaisin taulukoksi(array) */
 })
 
 /* Selaimessa: Hiiren oikea, inspect, (Pick element) label >> span */
